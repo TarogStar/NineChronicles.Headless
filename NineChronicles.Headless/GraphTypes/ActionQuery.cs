e3a3a7c1f0e8f638e5a48d82de4b315b0d85631e
Troy Spjute <tarogstar@gmail.com>
2022-09-13 10:06:46 -0600

    Add HackAndSlash ActionQuery

--- NineChronicles.Headless/GraphTypes/ActionQuery.cs
+++ NineChronicles.Headless/GraphTypes/ActionQuery.cs
@@ -144,6 +144,78 @@ namespace NineChronicles.Headless.GraphTypes
                     return Codec.Encode(action.PlainValue);
                 });
             Field<ByteStringType>(
+                name: "hackAndSlashAction",
+                description: "Start stage to get material.",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<AddressType>>
+                    {
+                        Name = "avatarAddress",
+                        Description = "Avatar address."
+                    },
+                    new QueryArgument<NonNullGraphType<IntGraphType>>
+                    {
+                        Name = "worldId",
+                        Description = "World ID containing the stage ID."
+                    },
+                    new QueryArgument<NonNullGraphType<IntGraphType>>
+                    {
+                        Name = "stageId",
+                        Description = "Stage ID."
+                    },
+                    new QueryArgument<ListGraphType<GuidGraphType>>
+                    {
+                        Name = "costumeIds",
+                        Description = "List of costume id for equip."
+                    },
+                    new QueryArgument<ListGraphType<GuidGraphType>>
+                    {
+                        Name = "equipmentIds",
+                        Description = "List of equipment id for equip."
+                    },
+                    new QueryArgument<ListGraphType<GuidGraphType>>
+                    {
+                        Name = "consumableIds",
+                        Description = "List of consumable id for use."
+                    },
+                    new QueryArgument<NonNullGraphType<IntGraphType>>
+                    {
+                        Name = "playCount",
+                        Description = "Number of times to play the level, defaults to 1.",
+                        DefaultValue = 1
+                    },
+                    new QueryArgument<IntGraphType>
+                    {
+                        Name = "buffId",
+                        Description = "id of the buff to use, must already be available.",
+                        DefaultValue = 1
+                    }
+                ),
+                resolve: context =>
+                {
+                    Address avatarAddress = context.GetArgument<Address>("avatarAddress");
+                    int worldId = context.GetArgument<int>("worldId");
+                    int stageId = context.GetArgument<int>("stageId");
+                    Address rankingMapAddress = context.GetArgument<Address>("rankingMapAddress");
+                    List<Guid> costumeIds = context.GetArgument<List<Guid>>("costumeIds") ?? new List<Guid>();
+                    List<Guid> equipmentIds = context.GetArgument<List<Guid>>("equipmentIds") ?? new List<Guid>();
+                    List<Guid> consumableIds = context.GetArgument<List<Guid>>("consumableIds") ?? new List<Guid>();
+                    var playCount = context.GetArgument<int>("playCount");
+                    var buffId = context.GetArgument<int?>("buffId");
+
+                    var action = new HackAndSlash
+                    {
+                        AvatarAddress = avatarAddress,
+                        WorldId = worldId,
+                        StageId = stageId,
+                        Costumes = costumeIds,
+                        Equipments = equipmentIds,
+                        Foods = consumableIds,
+                        PlayCount = playCount,
+                        StageBuffId = buffId,
+                    };
+                    return Codec.Encode(action.PlainValue);
+                });
+            Field<ByteStringType>(
                 name: "transferAsset",
                 arguments: new QueryArguments(
                     new QueryArgument<NonNullGraphType<AddressType>>
